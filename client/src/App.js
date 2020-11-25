ed9fa07265f67499c1d527714bcd5954962dcacd
Aman Bhardwaj <inblack>
2020-11-25 14:35:17 +0530

    bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+
+const App = () => {
+	return (
+		<div className='container'>
+			<h1>hello</h1>
+		</div>
+	);
+};
+
+export default App;
